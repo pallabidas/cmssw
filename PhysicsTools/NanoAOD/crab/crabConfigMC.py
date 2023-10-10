25e2fb2b9cbc44404184ff82f3d54037ca5e051a
Andrew Brinkerhoff <abrinke1@cern.ch>
2023-10-10 15:31:11 +0200

    AWB change default CRAB MC splitting to 10 LS

--- PhysicsTools/NanoAOD/crab/crabConfigMC.py
+++ PhysicsTools/NanoAOD/crab/crabConfigMC.py
@@ -17,6 +17,7 @@ config.General.transferLogs = True
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'test/Nano_MC_addHto4bPlus_crab_cfg.py'
 config.JobType.maxMemoryMB = 4000
+# config.JobType.priority = 100  ## High priority for small test
 
 # config.Data.inputDataset = '/SUSY_GluGluH_01J_HToAATo4B_Pt150_M-12_TuneCP5_13TeV_madgraph_pythia8/RunIISummer20UL18MiniAODv2-106X_upgrade2018_realistic_v16_L1v1-v1/MINIAODSIM'
 config.Data.inputDataset = 'DUMMY'
@@ -28,7 +29,7 @@ config.Data.publication = False
 
 config.Data.ignoreLocality = False
 config.Data.inputDBS = 'global'
-config.Data.splitting = 'Automatic' # 'Automatic' #'LumiBased' 'FileBased'
-# config.Data.unitsPerJob = 1
+config.Data.splitting = 'LumiBased' # 'Automatic' #'LumiBased' 'FileBased'
+config.Data.unitsPerJob = 10
 config.Site.storageSite = 'T2_CH_CERN' # Choose your site
 config.Data.outLFNDirBase = '/store/group/phys_susy/HToaaTo4b/NanoAOD/2018/MC/PNet_v1_2023_10_06/'
